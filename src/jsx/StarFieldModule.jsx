5f0aec92129954d4ba16b00c270f31635c39b99b
Mason Wong <masonwongcs@gmail.com>
2021-03-10 20:59:37 +0800

    Bug fixes based on QA Reports

--- src/jsx/StarFieldModule.jsx
+++ src/jsx/StarFieldModule.jsx
@@ -1,39 +1,50 @@
 import Utils from "./Utils.jsx";
 
 export default class StarFieldModule {
-    constructor(starCount) {
-        this.starCount = starCount;
-    }
+  constructor(starCount) {
+    this.starCount = starCount;
+  }
 
-    build() {
-        for (var i = 0; i < this.starCount; i++) {
-            new StarElement().start();
-        }
+  build() {
+    for (var i = 0; i < this.starCount; i++) {
+      new StarElement().start();
     }
+  }
 
-    start() {
-        this.build();
-    }
-
+  start() {
+    this.build();
+  }
 }
 
 class StarElement {
-    constructor() {
-        let sparkles = document.querySelector('.sparkles');
-        this.el = document.createElement('div');
-        this.el.className = 'sparkle';
-        this.x = Utils.randomBetween(1, sparkles.getBoundingClientRect().width);
-        this.y = Utils.randomBetween(1, sparkles.getBoundingClientRect().height);
-        sparkles.appendChild(this.el);
-        var w = 1;
+  constructor() {
+    let sparkles = document.querySelector(".sparkles");
+    this.el = document.createElement("div");
+    this.el.className = "sparkle";
+    this.x = Utils.randomBetween(1, sparkles.getBoundingClientRect().width);
+    this.y = Utils.randomBetween(1, sparkles.getBoundingClientRect().height);
+    sparkles.appendChild(this.el);
+    var w = 1;
 
-        TweenMax.set(this.el, { x: this.x, y: this.y, width: w, height: w, opacity: 0 });
-    }
+    TweenMax.set(this.el, {
+      x: this.x,
+      y: this.y,
+      width: w,
+      height: w,
+      opacity: 0,
+    });
+  }
 
-    start() {
-        TweenMax.to(this.el, Utils.randomBetween(5, 10) / 10, { opacity: 1, ease: Quad.easeInOut, repeat: -1, yoyo: true, delay: Utils.randomBetween(1, 50) / 10 });
+  start() {
+    TweenMax.to(this.el, Utils.randomBetween(5, 10) / 10, {
+      opacity: 1,
+      ease: Quad.easeInOut,
+      repeat: -1,
+      yoyo: true,
+      delay: Utils.randomBetween(1, 50) / 10,
+    });
 
-        // stop sparkling after 15 seconds
-        TweenMax.to(document.querySelector('.sparkle'), .5, {delay:15, opacity: 0, ease: Quad.easeInOut});
-    }
+    // stop sparkling after 15 seconds
+    TweenMax.to(this.el, 0.5, { delay: 15, opacity: 0, ease: Quad.easeInOut });
+  }
 }
--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -3,58 +3,82 @@ import StarFieldModule from "./StarFieldModule.jsx";
 import AnimationModule from "./AnimationModule.jsx";
 
 class Banner {
+  constructor() {
+    window.clickTAG = "http://mediamonks.com/richmedia/work";
+    const that = this;
+    this.preloadImages().then(() => {
+      that.setupEventListeners();
+      that.setupStarfield();
+    });
+  }
 
-    constructor() {
-        this.preloadImages()    
-        this.setupEventListeners();
-        this.setupStarfield();
-    }
+  preloadImages() {
+    return new Promise((resolve, reject) => {
+      let images = document.querySelectorAll("img");
+      let loaded = 0;
+      let promiseQueue = [];
 
-    preloadImages() {
-        let images = document.querySelectorAll('img');
-        let loaded = 0;
-            
-        // IE bug fix
-        for(let i = 0; i < images.length; i++) {
-            images[i].src = images[i].getAttribute('data-src');
-        }
-        
-    }
+      // IE bug fix
+      for (let i = 0; i < images.length; i++) {
+        images[i].src = images[i].getAttribute("data-src");
 
-    setupEventListeners() {
-        document.getElementsByClassName("bannerClick")[0].addEventListener("click", this.exit.bind(this));
-        document.getElementsByClassName("bannerClick")[0].addEventListener("mouseover", this.rollover.bind(this));
-        document.getElementsByClassName("bannerClick")[0].addEventListener("mouseout", this.rollout.bind(this));
-    }
+        const promise = new Promise((resolve) => {
+          const img = new Image();
+          img.onload = () => resolve();
+          img.onerror = () => resolve();
 
-    setupStarfield() {
-        let starfield = new StarFieldModule(300);
-        starfield.start();
-    }
+          img.src = images[i].src;
+        });
 
-    start() {
-        this.animation = new AnimationModule();
-        this.animation.start();
+        promiseQueue.push(promise);
+      }
 
-        const banner = document.querySelector("#banner");
-        banner.classList.remove('hide');
-    }
+      Promise.all(promiseQueue).then(() => {
+        resolve();
+      });
+    });
+  }
 
-    rollover() {
-        animation.playCtaIn();
-    };
-    
-    rollout() {
-        animation.playCtaOut();
-    };
+  setupEventListeners() {
+    document
+      .getElementsByClassName("bannerClick")[0]
+      .addEventListener("click", this.exit.bind(this));
+    document
+      .getElementsByClassName("bannerClick")[0]
+      .addEventListener("mouseover", this.rollover.bind(this));
+    document
+      .getElementsByClassName("bannerClick")[0]
+      .addEventListener("mouseout", this.rollout.bind(this));
+  }
 
-    exit() {
-        this.animation.stop();
-        window.open(window.clickTAG);
-    }
+  setupStarfield() {
+    let starfield = new StarFieldModule(300);
+    starfield.start();
+  }
+
+  start() {
+    this.animation = new AnimationModule();
+    this.animation.start();
+
+    const banner = document.querySelector("#banner");
+    banner.classList.remove("hide");
+  }
+
+  rollover() {
+    this.animation.playCtaIn();
+  }
+
+  rollout() {
+    this.animation.playCtaOut();
+  }
+
+  exit() {
+    this.animation.stop();
+    window.open(window.clickTAG);
+  }
 }
 
 window.addEventListener("load", () => {
-    let banner = new Banner();
-    banner.start();    
-})
+  let banner = new Banner();
+  banner.start();
+});
